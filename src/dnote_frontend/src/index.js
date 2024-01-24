356a08956a7457a3f4428498400ca52f18cb28cc
johnxiaohe <1154910941@qq.com>
2024-01-25 00:18:53 +0800

    :sparkles: 完成namespace相关内容

--- src/dnote_frontend/src/index.js
+++ src/dnote_frontend/src/index.js
@@ -1,7 +1,8 @@
 import { createActor,dnote_backend } from "../../declarations/dnote_backend";
+import { createActor as cUserActor} from "../../declarations/dnote_user";
 import {AuthClient} from "@dfinity/auth-client";
 import { HttpAgent } from "@dfinity/agent";
-import { createUserActor } from "./user";
+// import { createUserActor } from "./user";
 
 let mActor = dnote_backend;
 let uActor;
@@ -9,11 +10,20 @@ let uActor;
 let userInfo = undefined;
 let identity;
 
+const indexElm = document.getElementById("index");
 const loginButton = document.getElementById("login");
 const username = document.getElementById("username");
 const namespaceButton = document.getElementById("namespaces");
 const s_submit = document.getElementById("s_submit");
 const signup_form = document.getElementById("signup");
+signup_form.style.display = 'none';
+
+async function index(){
+  const idx = document.getElementById("idx");
+  const nsp = document.getElementById("nsp");
+  idx.style.display = 'block';
+  nsp.style.display = 'none';
+}
 
 async function login(e) {
   e.preventDefault();
@@ -32,13 +42,14 @@ async function login(e) {
 
   let userInfoResp = await mActor.getUserInfo();
   console.log(userInfoResp);
-  if (userInfoResp.code === 404){
+  if (userInfoResp.code == 404){
     // 用户尚未注册，弹窗提示注册信息。
-    signup_form.display='block';
-  }else if(userInfoResp.code === 200){
+    alert("请先提交基础信息");
+    signup_form.style.display = '';
+  }else if(userInfoResp.code == 200){
     userInfo = userInfoResp.data;
     console.log(userInfo);
-    uActor = createUserActor(identity, userInfo.id);
+    uActor = cUserActor(userInfo.id, {agent});
     // 登陆按钮隐藏，显示个人头像按钮
     loginButton.innerText = "Logout";
     username.innerText = userInfo.name;
@@ -48,20 +59,24 @@ async function login(e) {
 };
 
 async function namespaces(){
-  if(userInfo === undefined){
+  if(userInfo == undefined){
     alert("pls connect wallet");
     return;
   }
   const idx = document.getElementById("idx");
   const nsp = document.getElementById("nsp");
   idx.style.display = 'none';
-  nsp.style.display = 'block'
+  nsp.style.display = 'flex';
   const nsp_u = document.getElementById("nsp_u");
+  nsp_u.innerHTML = ''
   let namespaces = await uActor.namespaces();
-  for(ns in namespaces){
+  console.log(namespaces);
+  for(let ns of namespaces){
+    console.log(ns);
     let n_li = document.createElement("li");
     n_li.id=ns.id;
     n_li.innerText=ns.name;
+    n_li.onclick = () => ns_list(ns.id, ns.name);
     nsp_u.appendChild(n_li);
   }
   let n_li = document.createElement("li");
@@ -73,14 +88,13 @@ async function namespaces(){
 
 async function submit(){
   const n_username = document.getElementById("n_username");
-  let username = n_username.innerText;
-  if(username === ""){
+  let username = n_username.value;
+  if(username == ""){
     alert("pls enter username");
     return;
   }
   await mActor.sign(username, "", "a man");
-  alert("succcess");
-  signup_form.display = 'none';
+  signup_form.style.display = 'none';
   let userInfoResp = await mActor.getUserInfo();
   console.log(userInfoResp);
   userInfo = userInfoResp.data;
@@ -95,12 +109,36 @@ async function submit(){
 
 async function addNs(){
   let n_li = document.getElementById("n_ns");
+  n_li.onclick = null;
   // 录入名称、简介。提交创建。重载nsp页面
-  n_li.innerHTML = "<div></div>"
+  n_li.innerHTML = "";
+  n_li.innerHTML = "<div><lable>ns-name:</lable><input id=\"ns_name\" type=\"text\"/><lable>desc:</lable><input id=\"ns_desc\" type=\"text\"/><button id=\"ns_save\">save</button></div>"
+  let ns_save = document.getElementById("ns_save");
+  ns_save.onclick = createNs;
+}
+
+async function createNs(){
+  let ns_name = document.getElementById("ns_name");
+  let ns_desc = document.getElementById("ns_desc");
+  await uActor.createNamespace(ns_name.value, ns_desc.value, {"Public": null} );
+  alert("create success!");
+  await namespaces();
 }
 
 // 指定命名空间的文章列表
-async function ns_list(){
+async function ns_list(id,name){
+  let c_ns = document.getElementById("c_ns");
+  let ats = document.getElementById("ats");
+  c_ns.innerText = name;
+  c_ns.onclick = () => {     
+    const idx = document.getElementById("idx");
+    const nsp = document.getElementById("nsp");
+    idx.style.display = 'none';
+    nsp.style.display = 'none';
+ };
+  ats.style.display='flex';
+  const nsp = document.getElementById("nsp");
+  nsp.style.display = 'none';
 
 }
 
@@ -114,6 +152,7 @@ async function artical(){
   
 }
 
+indexElm.onclick = index;
 loginButton.onclick = login;
 namespaceButton.onclick = namespaces;
 s_submit.onclick = submit;
